406356cbc0de86f48ef998cd4b1d8a24579e7a79
Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>
2024-06-20 21:28:47 +0530

    Update event.js

--- controller/event.js
+++ controller/event.js
@@ -96,7 +96,7 @@ router.delete(
         return next(new ErrorHandler("Product is not found with this id", 404));
       }    
 
-      for (let i = 0; 1 < product.images.length; i++) {
+      for (let i = 0; i < product.images.length; i++) {
         const result = await cloudinary.v2.uploader.destroy(
           event.images[i].public_id
         );
